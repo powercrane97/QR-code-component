07e440fe4fe5a116bd508a98de0eeb5f71d4af73
rarnold <rarnold@student.21-school.ru>
2023-01-27 17:57:10 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id='app'>
+      <div className='wrap'>
+        <img
+          src='/images/image-qr-code.png'
+          alt='QR-Code with link to frontendmentor.io'
+        />
+        <div className='description'>
+          <h2>Improve your front-end skills by building projects</h2>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
